e9218c37393662a3012109c32f1b1eb45682ba34
AukeMeijer <a.meijer@alfa-college.nl>
2025-02-15 09:11:41 +0100

    Build upon kickstart

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,35 +1,67 @@
-require "nvchad.options"
+-- [[ Setting options ]]
+-- See `:help vim.opt`
+-- NOTE: You can change these options as you wish!
+--  For more options, you can see `:help option-list`
 
--- add yours here!
-local o = vim.o
+-- Make line numbers default
+vim.opt.number = true
+-- You can also add relative line numbers, to help with jumping.
+--  Experiment for yourself to see if you like it!
+vim.opt.relativenumber = true
 
--- Indenting
-o.shiftwidth = 4
+-- Enable mouse mode, can be useful for resizing splits for example!
+vim.opt.mouse = 'a'
 
-o.tabstop = 4
+-- Don't show the mode, since it's already in the status line
+vim.opt.showmode = false
 
-o.softtabstop = 4
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
 
-o.number = true
+-- Enable break indent
+vim.opt.breakindent = true
 
-o.relativenumber = true
+-- Save undo history
+vim.opt.undofile = true
 
-o.splitbelow = true
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
-o.splitright = true
+-- Keep signcolumn on by default
+vim.opt.signcolumn = 'yes'
 
-o.wrap = false
+-- Decrease update time
+vim.opt.updatetime = 250
 
-o.clipboard = "unnamedplus"
+-- Decrease mapped sequence wait time
+vim.opt.timeoutlen = 300
 
-o.scrolloff = 999
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
 
-o.virtualedit = "block"
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
-o.inccommand = "split"
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = 'split'
 
-o.ignorecase = true
+-- Show which line your cursor is on
+vim.opt.cursorline = true
 
-o.termguicolors = true
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
 
-o.cursorlineopt = "both" -- to enable cursorline!
+vim.opt.virtualedit = 'block'
+
+vim.opt.termguicolors = true
+-- vim: ts=2 sts=2 sw=2 et
--- .wezterm.lua
+++ .wezterm.lua
@@ -6,7 +6,6 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
-
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
 config.font_size = 19
 
@@ -16,6 +15,7 @@ config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.9
 config.macos_window_background_blur = 10
 
-config.color_scheme = 'Catppuccin Mocha'
+config.color_scheme = "Catppuccin Mocha"
 -- and finally, return the configuration to wezterm
+
 return config
